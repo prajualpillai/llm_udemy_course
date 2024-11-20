1f952fa28bb64824edad9f4ad1c8207d93409d8d
ndh00961 <prajual.pillai@nissanmotor.com>
2024-11-20 09:01:34 +0530

    Finished course

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 groq_key = "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"
 proxy = "http://<ndh00961>:<a9450930135A!>@10.203.32.190:8080"
+ser_api = "a8ffafd47dd5e83b835e64a761e1f8f2980bcd003762322ba1fcaaf5b0da449e"
